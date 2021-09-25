c503fc410a7f859fe03c32dc898f2b3c29c8703f
Sebastian <sebastian@pop-os.localdomain>
2021-09-25 12:58:00 +0200

    fixing import in user.service.ts

--- src/app/flight.service.ts
+++ src/app/flight.service.ts
@@ -3,18 +3,19 @@ import {HttpClient} from "@angular/common/http";
 import {Flight} from "./shared/models/flight.model";
 import {Observable} from "rxjs";
 import {contentHeaders} from "./http-config";
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: "root",
 })
 export class FlightService {
-  private apiUrl = 'http://localhost:8085'
+  // private apiUrl = 'http://localhost:8085'
 
 
   constructor(private httpClient: HttpClient) {
   }
 
   getAllFlights(): Observable<[Flight]> {
-    return this.httpClient.get<[Flight]>(this.apiUrl + '/allFlights', {headers: contentHeaders})
+    return this.httpClient.get<[Flight]>(environment.backendUrl + '/allFlights', {headers: contentHeaders})
   }
 }
