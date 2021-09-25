8c06b15db82acba814fe8e3243a71d85e0cbbdee
Sebastian Gronek <groneks.a@gmail.com>
2021-09-25 11:52:12 +0200

    fixing url again in server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Rezerwacja-Biletow-Lotniczych-Frontend'));
+app.use(express.static(__dirname + '/dist/Rezerwacje-Biletow-Lotniczych-Frontend'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/Rezerwacja-Biletow-Lotniczych-Frontend/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/Rezerwacje-Biletow-Lotniczych-Frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
