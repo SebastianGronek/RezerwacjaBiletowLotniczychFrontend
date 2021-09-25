bf7d0828e088bdf947399c5e7f620e381f93a557
Sebastian Gronek <groneks.a@gmail.com>
2021-09-25 12:08:30 +0200

    added listing all users

--- src/app/router-config.config.ts
+++ src/app/router-config.config.ts
@@ -1,9 +1,11 @@
 import {Routes} from "@angular/router";
 import {UserAddComponent} from "./user-add/user-add.component";
 import {FlightListComponent} from "./flight-list/flight-list.component";
+import {UserListComponent} from "./user-list/user-list.component";
 
 export const routerConfig: Routes = [
   {path: 'addUser', component:UserAddComponent},
+  {path: 'allUsers', component:UserListComponent},
   {path: 'allFlights', component:FlightListComponent},
 /*  {path: '', redirectTo: ""},*/
 ]
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -3,17 +3,21 @@ import {HttpClient} from "@angular/common/http";
 import {User} from "./shared/models/user.model";
 import {Observable} from "rxjs";
 import {contentHeaders} from "./http-config";
+import {environment} from "../environments/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  private backendUrl = 'http://localhost:8085'
 
   constructor(private httpClient: HttpClient) {
   }
 
   create(user: User): Observable<User> {
-    return this.httpClient.post<User>(this.backendUrl + '/addUser', user, {headers: contentHeaders})
+    return this.httpClient.post<User>(environment.backendUrl + '/addUser', user, {headers: contentHeaders})
+  }
+
+  getAllUsers(): Observable<[User]> {
+    return this.httpClient.get<[User]>(environment.backendUrl + '/allUsers', {headers: contentHeaders})
   }
 }
