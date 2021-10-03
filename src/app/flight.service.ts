63a384c0aa95822fc735a5f4961c57b1e244202b
Sebastian <sebastian@pop-os.localdomain>
2021-10-03 14:54:25 +0200

    added autocomplete field starting location in form

--- src/app/flight.service.ts
+++ src/app/flight.service.ts
@@ -25,4 +25,9 @@ export class FlightService {
       .set('time', dateOfFlight)
     return this.httpClient.get<[Flight]>(environment.backendUrl + '/findFlight', {params}/*, {headers: contentHeaders}*/)
   }
+
+  getLocationFilteredByName(input: string, url: string): Observable<[string]> {
+    const params = new HttpParams().set('input', input)
+    return this.httpClient.get<[string]>(environment.backendUrl + "/getAll"+url, {params});
+  }
 }
