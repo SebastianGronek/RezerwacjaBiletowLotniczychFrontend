ec5810b5f29e1d9f11ec06523dbc065fd7e24f31
Sebastian Gronek <groneks.a@gmail.com>
2021-10-03 15:45:44 +0200

    cleaning user in master

--- src/app/shared/models/user.model.ts
+++ src/app/shared/models/user.model.ts
@@ -1,11 +1,8 @@
-import {Ticket} from "./ticket.model";
-
 export class User {
   userId: number;
   userFirstName: string;
   userLastName: string;
   login: string;
-  ticketList?: [Ticket];
 
 
   constructor(id: number, userFirstName: string, userName: string, login: string) {
